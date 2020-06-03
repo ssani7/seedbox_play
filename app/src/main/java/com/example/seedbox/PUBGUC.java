7bcaef6d5dec241a804f8b1fbea534d4653db544
Evaan321 <evaanrahman@gmail.com>
2020-06-03 18:53:36 +0600

    CHODNA  !!!!!!

---
+++ app/src/main/java/com/example/seedbox/PUBGUC.java
@@ -0,0 +1,14 @@
+package com.example.seedbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PUBGUC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_u_b_g_u_c);
+    }
+}
