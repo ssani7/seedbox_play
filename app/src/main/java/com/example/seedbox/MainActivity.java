1dd6bf122029cc322cfbf5edf390483010a73516
ssani7 <66258879+ssani7@users.noreply.github.com>
2020-06-04 20:41:12 +0600

    menubar added

--- app/src/main/java/com/example/seedbox/MainActivity.java
+++ app/src/main/java/com/example/seedbox/MainActivity.java
@@ -1,23 +1,75 @@
 package com.example.seedbox;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.seedbox.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_home,
+                new Home()).commit();
+        navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_home,
+                        new Home()).commit();
+                break;
+            case R.id.nav_seedboxplan:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_home,
+                        new seedboxsection()).commit();
+                break;
+            case R.id.nav_COC:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_home,
+                        new coc()).commit();
+                break;
+            case R.id.nav_uc:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_home,
+                        new PUBGUC()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/seedbox/PUBGUC.java
+++ app/src/main/java/com/example/seedbox/PUBGUC.java
@@ -1,14 +1,22 @@
 package com.example.seedbox;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class PUBGUC extends AppCompatActivity {
+public class PUBGUC extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_p_u_b_g_u_c);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_p_u_b_g_u_c, container, false);
     }
+
+
 }
