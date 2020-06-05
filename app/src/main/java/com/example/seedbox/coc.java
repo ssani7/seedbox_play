a951a63a6f894a2577d610d03ac06428b50c06e6
ssani7 <66258879+ssani7@users.noreply.github.com>
2020-06-05 21:09:09 +0600

    converter final

--- app/src/main/java/com/example/seedbox/coc.java
+++ app/src/main/java/com/example/seedbox/coc.java
@@ -1,14 +1,20 @@
 package com.example.seedbox;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class COC extends AppCompatActivity {
+public class coc extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_c_o_c);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_coc, container, false);
     }
 }
