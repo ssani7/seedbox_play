1bb31e1fe3cdf5dd711561655994ac120f900183
ssani7 <66258879+ssani7@users.noreply.github.com>
2020-06-07 16:22:40 +0600

    ad

--- app/src/main/java/com/example/seedbox/MainActivity.java
+++ app/src/main/java/com/example/seedbox/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.ViewFlipper;
 
 
 import com.example.seedbox.R;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -36,10 +37,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        MobileAds.initialize(this, "ca-app-pub-5024069063645891~5346190944");
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
         maddie = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         maddie.loadAd(adRequest);
+
+        maddie.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
 
 
         drawer = findViewById(R.id.drawer_layout);
